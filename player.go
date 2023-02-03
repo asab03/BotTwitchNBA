d14b36bc32e6009baae9f9358e3adf7b69865b29
asab03 <asabella92@gmail.com>
2023-02-03 13:44:15 +0100

    Recuperation des matchs du jour

--- player.go
+++ player.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type JsonPlayer struct {
@@ -47,7 +48,86 @@ type JsonPlayer struct {
 		} `json:"leagues"`
 	} `json:"response"`
 }
-//var client *http.Client
+
+type JsonMatchOfTheDay struct {
+	Get        string `json:"get"`
+	Parameters struct {
+		Date string `json:"date"`
+	} `json:"parameters"`
+	Errors   []interface{} `json:"errors"`
+	Results  int           `json:"results"`
+	Response []struct {
+		ID     int    `json:"id"`
+		League string `json:"league"`
+		Season int    `json:"season"`
+		Date   struct {
+			Start    time.Time   `json:"start"`
+			End      interface{} `json:"end"`
+			Duration interface{} `json:"duration"`
+		} `json:"date"`
+		Stage  int `json:"stage"`
+		Status struct {
+			Clock    interface{} `json:"clock"`
+			Halftime bool        `json:"halftime"`
+			Short    int         `json:"short"`
+			Long     string      `json:"long"`
+		} `json:"status"`
+		Periods struct {
+			Current     int  `json:"current"`
+			Total       int  `json:"total"`
+			EndOfPeriod bool `json:"endOfPeriod"`
+		} `json:"periods"`
+		Arena struct {
+			Name    interface{} `json:"name"`
+			City    interface{} `json:"city"`
+			State   interface{} `json:"state"`
+			Country interface{} `json:"country"`
+		} `json:"arena"`
+		Teams struct {
+			Visitors struct {
+				ID       int    `json:"id"`
+				Name     string `json:"name"`
+				Nickname string `json:"nickname"`
+				Code     string `json:"code"`
+				Logo     string `json:"logo"`
+			} `json:"visitors"`
+			Home struct {
+				ID       int    `json:"id"`
+				Name     string `json:"name"`
+				Nickname string `json:"nickname"`
+				Code     string `json:"code"`
+				Logo     string `json:"logo"`
+			} `json:"home"`
+		} `json:"teams"`
+		Scores struct {
+			Visitors struct {
+				Win    int `json:"win"`
+				Loss   int `json:"loss"`
+				Series struct {
+					Win  int `json:"win"`
+					Loss int `json:"loss"`
+				} `json:"series"`
+				Linescore []string `json:"linescore"`
+				Points    int      `json:"points"`
+			} `json:"visitors"`
+			Home struct {
+				Win    int `json:"win"`
+				Loss   int `json:"loss"`
+				Series struct {
+					Win  int `json:"win"`
+					Loss int `json:"loss"`
+				} `json:"series"`
+				Linescore []string `json:"linescore"`
+				Points    int      `json:"points"`
+			} `json:"home"`
+		} `json:"scores"`
+		Officials   []interface{} `json:"officials"`
+		TimesTied   interface{}   `json:"timesTied"`
+		LeadChanges interface{}   `json:"leadChanges"`
+		Nugget      interface{}   `json:"nugget"`
+	} `json:"response"`
+}
+
 
 func PrettyPrint(i interface{}) string {
     s, _ := json.MarshalIndent(i, "", "\t")
@@ -56,7 +136,7 @@ func PrettyPrint(i interface{}) string {
 
 func main(){
 
-
+	// Definir le joueur à rechercher
 	var player string
 	var firstName string
 	fmt.Println("Prenom du joueur :")
@@ -64,6 +144,8 @@ func main(){
 	fmt.Println("Nom du joueur :")
 	fmt.Scan(&player)
 	fmt.Println(firstName, player) 
+
+	//recherche du joueur dans l'api pour definir son Id
 
 	url := "https://api-nba-v1.p.rapidapi.com/players?search=" + player
 
@@ -77,11 +159,11 @@ func main(){
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 
-	fmt.Println(res)
-	fmt.Println(string(body))
+	//fmt.Println(res)
+	//fmt.Println(string(body))
 
 	var result JsonPlayer
-    if err := json.Unmarshal(body, &result); err != nil {  // Parse []byte to the go struct pointer
+    if err := json.Unmarshal(body, &result); err != nil { 
         fmt.Println("Can not unmarshal JSON")
     }
 
@@ -100,7 +182,42 @@ func main(){
 	}
 	fmt.Println(playerId, playerFirstName, playerLastName)
 
+	//recherche du match dans l'api pour obtenir Id
 	
+	dt :=  time.Now().Local().Format("2006-01-02")
+	
+
+	fmt.Println(dt)
+	
+
+	url2 := "https://api-nba-v1.p.rapidapi.com/games?date=" + dt
+
+	fmt.Println(url2)
+
+	req2, _ := http.NewRequest("GET", url2, nil)
+
+	req2.Header.Add("X-RapidAPI-Key", "de18451645msh049760fce527e8fp1e555ejsn4eb253213e47")
+	req2.Header.Add("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
+
+	res2, _ := http.DefaultClient.Do(req2)
+
+	defer res2.Body.Close()
+	body2, _ := ioutil.ReadAll(res2.Body)
+
+	//fmt.Println(res2)
+	//fmt.Println(string(body2))
+
+	var result2 JsonMatchOfTheDay
+    if err2 := json.Unmarshal(body2, &result2); err2 != nil { 
+        fmt.Println("Can not unmarshal JSON")
+    }
+
+	for _, rec2 := range result2.Response{
+		fmt.Println("L'Id du match :", rec2.ID)		
+		fmt.Println("Visiteur :" , rec2.Teams.Visitors.Name, "score :", rec2.Scores.Visitors.Points)
+		fmt.Println("Domicile :" , rec2.Teams.Home.Name, "score :", rec2.Scores.Home.Points)
+	}
+
 	//recherche les stats dans l'api
 
 	//affiche les stats
